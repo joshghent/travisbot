d0da4d086297d80ad7086f5f541b80eb2af43e4b
Josh Ghent <me@joshghent.com>
2018-12-03 14:43:28 +0000

    Added php and python. Started code cov implementation

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # :robot: Travis Bot
 
-A bot for generating travis ci configs for any language
+> A bot for generating travis ci configs for any language
+
+## Install
+
+```
+$ npm install --global @joshghent/travisbot
+```
+
+## Usage
+```
+$ travisbot generate
+```
+You will be asked a series of questions and then it will generate you a `.travis.yml` file in the directory you are in
--- src/cli.ts
+++ src/cli.ts
@@ -11,11 +11,19 @@ enum languages {
     Python = "Python"
 }
 
+enum codeCoverageTools {
+    None = "None",
+    CodeCovIo = "Codecov.io",
+    CodeClimate = "Code Climate",
+    Coveralls = "Coveralls"
+}
+
 commander
     .command('generate')
     .description('Generate a travisCI config for a certain language')
     .alias('g')
     .action(() => {
+        let content = '';
         inquirer.prompt([{
             type: 'list',
             name: 'lang',
@@ -23,15 +31,35 @@ commander
             choices: Object.keys(languages).map(k => languages[k as any])
         }])
         .then((result: any) => {
-            console.log(`🌍 Using ${result.lang} for project`)
+            console.log(`🌍 Using ${result.lang} for project`);
 
             if (result.lang === languages.Node) {
-                nodeHandler();
+                content = nodeHandler();
             } else if (result.lang === languages.Php) {
-                phpHandler();
+                content = phpHandler();
             } else if (result.lang === languages.Python) {
-                pythonHandler();
+                content = pythonHandler();
             }
+
+            inquirer.prompt([{
+                type: 'list',
+                name: 'codecov',
+                message: 'Which code coverage tool do you use?',
+                choices: Object.keys(codeCoverageTools).map(k => codeCoverageTools[k as any])
+            }])
+            .then((result: any) => {
+                if (result.codecov !== codeCoverageTools.None) {
+                    console.log("Please make sure the code coverage command is run as part of your test command!");
+                }
+
+                if (result.codecov === codeCoverageTools.CodeCovIo) {
+                    content += "\n\n" + `after_success:
+        - bash <(curl -s https://codecov.io/bash)`;
+                }
+
+                fs.writeFileSync('.travis.yml', tabsToSpaces(content, 2));
+                console.log(`⭐ Finishing writing travis config`);
+            })
         });
     });
 
@@ -39,33 +67,27 @@ commander
     .version(require("./../package.json").version)
     .parse(process.argv);
 
-function nodeHandler(): void {
+function nodeHandler(): string {
     const content = `language: node_js
     node_js:
       - "node"
       - "10"
       - "8"
       - "lts/*"`;
-    fs.writeFileSync('.travis.yml', tabsToSpaces(content, 4));
-    console.log(`⭐ Finishing writing travis config`);
+
+    return content;
 }
 
-const phpHandler = () => {
-    return true;
+function phpHandler(): string {
+    return `language: php
+    php:
+        - 7
+        - 5
+        - hhvm`;
 }
-const pythonHandler = () => {
-    return true;
+function pythonHandler(): string {
+    return `language: python
+    python:
+        - 2
+        - 3`;
 }
-
-// Ask what language they are using
-
-// If Node
-    // Do you use yarn or npm?
-// If PHP
-    // Do you use composer?
-// If Python
-    // Which versions? (option 2 or 3)
-    // Test command?
-
-// No matter what language
-    // Specify source for environment variables
